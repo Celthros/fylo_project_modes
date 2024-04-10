dc4a1dc3b1138d1d5d047d75beee54f9d0749cae
tyler <mr.anderson@realtourvision.com>
2024-04-10 07:41:42 -0400

    started fylo project tailwindcss dark/light modes

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// https://www.freecodecamp.org/news/how-to-build-a-dark-mode-switcher-with-tailwind-css-and-flowbite
+// js code in header
+//// It's best to inline this in `head` to avoid FOUC (flash of unstyled content) when changing pages or themes
+
+const themeToggleBtn = document.getElementById("theme-toggle");
+const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Fylo Website
+
+Fylo landing page with dark/light mode switcher using tailwindcss from [Frontend Mentor Challenge](https://www.frontendmentor.io/challenges/loopstudios-landing-page-N88J5Onjw)
+
+![Alt text](images/fylo.png)
