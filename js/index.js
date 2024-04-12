e5eeb11772b007d25090c915c8f3c4f245cc4cb7
tyler <mr.anderson@realtourvision.com>
2024-04-11 22:26:56 -0400

    added color js

--- js/index.js
+++ js/index.js
@@ -5,3 +5,44 @@
 const themeToggleBtn = document.getElementById("theme-toggle");
 const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
 const themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  if (themeToggleLightIcon.classList.contains("hidden")) {
+    themeToggleLightIcon.classList.remove("hidden");
+  }
+} else {
+  if (themeToggleDarkIcon.classList.contains("hidden")) {
+    themeToggleDarkIcon.classList.remove("hidden");
+  }
+}
+
+const toggleMode = () => {
+  themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon.classList.toggle("hidden");
+
+  // if is set in localStorage
+  if (localStorage.getItem("color-theme")) {
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+  } else {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    }
+  }
+};
+
+//listen for toggle button click event
+themeToggleBtn.addEventListener("click", toggleMode);
